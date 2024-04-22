af7e932766d331dd72ddbb9bee3f3e4ca3e8cd49
Paulo Ricardo <pauloricardoreis28@gmail.com>
2024-04-22 07:38:31 -0300

    Organizando em pastas

---
+++ Exercícios-Python/ex021.py
@@ -0,0 +1,5 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('Exercícios-Python/ex.021b.mp3.mp3')
+pygame.mixer.music.play()
+input(pygame.event.wait())
--- ex021.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load('Meusprogetos2/murilohuff-dois-enganados-feat-marilia-mendonca-1d1dd61d.mp3')
-pygame.mixer.music.play()
-input(pygame.event.wait())
