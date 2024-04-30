4096be52be7f854aef1be1ed461619f0801ea469
Paulo Ricardo <pauloricardoreis28@gmail.com>
2024-04-29 21:06:09 -0300

    exercícios

--- Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex036.py
+++ Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex036.py
@@ -1,13 +1,15 @@
 print('-=-' * 20)
 print('EMPRÉSTIMO BANCÁRIO')
 print('-=-' * 20)
-casa = float(input('Qual o valor da casa que você quer comprar? R$'))
-salario = float(input('Qual o seu salário? R$'))
-tempo = float(input('Em quantos anos você pretende pagar? '))
+casa = float(input('Qual o valor da casa? R$'))
+salario = float(input('Qual o salário do comprador? R$'))
+tempo = float(input('Quantos anos de financiamento? '))
 
-tempo = tempo * 12
-parcela = casa / tempo
+meses = tempo * 12
+parcela = casa / meses
 if parcela <= (salario * 0.3):
+    print(f'Para pagar uma casa de {casa} em {tempo} anos a prestação será de R${parcela:.2f}')
     print('Parabéns! Sua solicitação de empréstimo foi aprovada.')
 else:
+    print(f'Para pagar uma casa de {casa} em {tempo} anos a prestação será de R${parcela:.2f}')
     print('Infelizmente você não atende aos requisitos mínimos para esse empréstimo.')
--- Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex042.py
+++ Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex042.py
@@ -3,10 +3,15 @@ r2 = float(input('Segunda reta: '))
 r3 = float(input('Terceira reta: '))
 c = 0
 if (r1 + r2) > r3 and (r1 + r3) > r2 and (r2 + r3) > r1:
-    print('As retas acima PODEM formar um triângulo! ')
+    print('As retas acima PODEM formar um triângulo! ', end= ' ')
     c = 1
 else:
     print('As retas acima NÃO PODEM formar um triângulo! ')
 if c==1:
     print('Elas formam um triângulo: ')
-    if r1
+if r1==r2==r3:
+    print('EQUILÁTERO')
+elif r1!=r2!=r3:
+    print('ESCALENO')
+else:
+    print('ISÓSCELES')
