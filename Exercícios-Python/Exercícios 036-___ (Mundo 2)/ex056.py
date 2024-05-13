52b72a6161e3d94e5d2aeabbc4ebe89fad9392e1
Paulo Ricardo <pauloricardoreis28@gmail.com>
2024-05-12 22:41:03 -0300

    correção do ex056

--- Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex056.py
+++ Exercícios-Python/Exercícios 036-___ (Mundo 2)/ex056.py
@@ -9,16 +9,16 @@ for c in range(0, 4):
     print(f'----- {c+1}ª PESSOA -----')
     nome = input('Nome: ')
     idade = int(input('Idade: '))
-    sexo = input('Sexo [M/F]: ')
+    sexo = input('Sexo [M/F]: ').strip()
     idades += idade
     if sexo == 'M':
         if idade > maior:
             maior = idade
             maisvelho = nome
     else:
-        if idade < 21:
+        if idade < 20:
             menos += 1
-m = idades // 4
-print(f'A média de idade do grupo é {m} \n'
-      f'O nome do homem mais velho é {maisvelho}. \n'
-      f'A quantidade de mulheres com menos de 21 anos é {menos}')
+m = idades / 4
+print(f'A média de idade do grupo é {m:.1f} \n'
+      f'O nome do homem mais velho tem {maior} anos e se chama {maisvelho}. \n'
+      f'A quantidade de mulheres com menos de 20 anos é {menos}')
